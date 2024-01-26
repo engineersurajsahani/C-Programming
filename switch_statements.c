19b68dfa85e4fb43769f53abc0574376858d148a
Suraj Sahani <93707688+techsurajsahani@users.noreply.github.com>
2024-01-26 19:19:14 +0530

    26/01/2024

---
+++ switch_statements.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    int day=1;
+    
+    switch(day){
+        
+        case 1:
+        printf("Monday");
+        break;
+        
+        case 2:
+        printf("Tuesday");
+        break;
+        
+        case 3:
+        printf("Wednesday");
+        break;
+        
+        case 4:
+        printf("Thursday");
+        break;
+        
+        case 5:
+        printf("Friday");
+        break;
+        
+        case 6:
+        printf("Saturday");
+        break;
+        
+        case 7:
+        printf("Saturday");
+        break;
+        
+        default:
+        printf("NOT");
+    }
+
+    return 0;
+}
