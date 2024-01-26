01db9826ae6389995192f941618507fb87444b85
Suraj Sahani <93707688+techsurajsahani@users.noreply.github.com>
2024-01-26 19:16:28 +0530

    26/01/2024

---
+++ if_else_conditional_statements.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int day=1;
+    
+    if(day==1){
+        printf("Monday");
+    }
+    else if(day==2){
+        printf("Tuesday");
+    }
+    else if(day==3){
+        printf("Wednesday");
+    }
+    else if(day==4){
+        printf("Thursday");
+    }
+    else if(day==5){
+        printf("Friday");
+    }
+    else if(day==6){
+        printf("Saturday");
+    }
+    else if(day==7){
+        printf("Sunday");
+    }
+    else{
+        printf("NOT");
+    }
+
+    return 0;
+}
